fbc39b162946bfcd2ac1449af0a954bda04ffaf8
NIKHIL KUMAR YADAV <152734681+Nikh1lXDev@users.noreply.github.com>
2026-07-27 10:03:02 +0530

    Time: 77 ms (78.4%), Space: 44.4 MB (53.49%) - CodeHub

---
+++ leetcode/0744-network-delay-time/0744-network-delay-time.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+
+        vector<vector<pair<int,int>>> adj(n + 1);
+
+        for(auto &edge : times){
+
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            adj[u].push_back({v, wt});
+        }
+
+        priority_queue<pair<int,int>,
+                       vector<pair<int,int>>,
+                       greater<pair<int,int>>> pq;
+
+        vector<int> dist(n + 1, INT_MAX);
+
+        dist[k] = 0;
+        pq.push({0, k});
+
+        while(!pq.empty()){
+
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            if(dis > dist[node])
+                continue;
+
+            for(auto it : adj[node]){
+
+                int neighbour = it.first;
+                int weight = it.second;
+
+                if(dis + weight < dist[neighbour]){
+
+                    dist[neighbour] = dis + weight;
+                    pq.push({dist[neighbour], neighbour});
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for(int i = 1; i <= n; i++){
+
+            if(dist[i] == INT_MAX)
+                return -1;
+
+            ans = max(ans, dist[i]);
+        }
+
+        return ans;
+    }
+};
