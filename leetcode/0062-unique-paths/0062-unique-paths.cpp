2a2e8887a14333679309ae349b151ddb140b8970
NIKHIL KUMAR YADAV <152734681+Nikh1lXDev@users.noreply.github.com>
2026-07-29 11:27:02 +0530

    Time: 0 ms (100%), Space: 9.5 MB (5.32%) - CodeHub

---
+++ leetcode/0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    int FindAns(int m, int n, vector<vector<int>>& dp){
+
+        if(m == 0 && n == 0){
+            return 1;
+        }
+
+        if(m < 0 || n < 0){
+            return 0;
+        }
+
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+
+        int up = FindAns(m - 1, n, dp);
+        int left = FindAns(m, n - 1, dp);
+
+        return dp[m][n] = up + left;
+    }
+
+    int uniquePaths(int m, int n){
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        return FindAns(m - 1, n - 1, dp);
+    }
+};
