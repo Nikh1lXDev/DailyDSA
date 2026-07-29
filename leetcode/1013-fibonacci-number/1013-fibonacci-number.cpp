3539245e5efd575b52f131565f02e8d59b6844af
NIKHIL KUMAR YADAV <152734681+Nikh1lXDev@users.noreply.github.com>
2026-07-29 09:59:51 +0530

    Time: 0 ms (100%), Space: 8.2 MB (16.77%) - CodeHub

--- leetcode/1013-fibonacci-number/1013-fibonacci-number.cpp
+++ leetcode/1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int fib(int n, vector<int>& dp) {
+    int fib(int n) {
 
         if(n <= 1){
             return n;
         }
 
-        if(dp[n] != -1){
-            return dp[n];
+        vector<int> dp(n + 1, 0);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        return dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
-    }
-
-    int fib(int n) {
-
-        vector<int> dp(n + 1, -1);
-
-        return fib(n, dp);
+        return dp[n];
     }
 };
