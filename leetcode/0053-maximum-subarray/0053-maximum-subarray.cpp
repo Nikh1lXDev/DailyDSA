1819a4adfc035c72cc2ec40449e383b962bf5443
NIKHIL KUMAR YADAV <152734681+Nikh1lXDev@users.noreply.github.com>
2026-09-01 21:48:13 +0530

    Time: 0 ms (100%), Space: 71.8 MB (14.88%) - CodeHub

---
+++ leetcode/0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int curr = nums[0];
+        int maxs = nums[0];
+
+        for(int i = 1; i < nums.size(); i++) {
+
+            curr = max(nums[i], curr + nums[i]);
+
+            maxs = max(maxs, curr);
+        }
+
+        return maxs;
+    }
+};
